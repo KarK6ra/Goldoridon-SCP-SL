67946f3dd740a684431d3cf727f3befb4b6a6261
KarKara <bureevvlad2@gmail.com>
2026-09-03 17:39:51 +0300

    инвентарь-контрол

---
+++ LabAPI/plugins/1984/InventoryControl-main/InventoryControl/EventsHandler.cs
@@ -0,0 +1,121 @@
+﻿using InventorySystem.Items;
+using InventorySystem.Items.Firearms;
+using InventorySystem.Items.Firearms.Attachments;
+using InventorySystem.Items.Firearms.Modules;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Features.Console;
+using LabApi.Features.Wrappers;
+using MEC;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+namespace InventoryControl
+{
+    public class EventsHandler
+    {
+        public void OnPlayerChangedRole(PlayerChangedRoleEventArgs ev)
+        {
+            try
+            {
+                if (!Round.IsRoundStarted || ev.Player == null || (ev.SpawnFlags == RoleSpawnFlags.None || ev.SpawnFlags == RoleSpawnFlags.UseSpawnpoint)) return;
+
+                Timing.CallDelayed(0.01f, () =>
+                {
+                    if (!ev.Player.IsDummy && InventoryControl.Instance.Config.InventoryRank?.Count > 0 && InventoryControl.Instance.Config.InventoryRank.ContainsKey(GetPlayerGroupName(ev.Player)))
+                        if (InventoryControl.Instance.Config.InventoryRank[GetPlayerGroupName(ev.Player)]?.Count(x => x.Value.RoleTypeId == ev.NewRole.RoleTypeId) > 0)
+                        { SetPlayerInventory(ev.Player, InventoryControl.Instance.Config.InventoryRank[GetPlayerGroupName(ev.Player)].Where(x => x.Value.RoleTypeId == ev.NewRole.RoleTypeId).ToList().RandomItem().Value); return; }
+                    if (InventoryControl.Instance.Config.Inventory?.Count(x => x.Value.RoleTypeId == ev.NewRole.RoleTypeId) > 0)
+                        SetPlayerInventory(ev.Player, InventoryControl.Instance.Config.Inventory.Where(x => x.Value.RoleTypeId == ev.NewRole.RoleTypeId).ToList().RandomItem().Value);
+                });
+            }
+            catch (Exception e)
+            {
+                Logger.Error("[Event: OnPlayerChangedRole] " + e.ToString());
+            }
+        }
+
+        private void SetPlayerInventory(Player player, RoleInventory roleInventory)
+        {
+            try
+            {
+                Dictionary<ItemType, ushort> Ammos = new Dictionary<ItemType, ushort>(player.Ammo);
+                if (!roleInventory.KeepItems && !player.IsWithoutItems) player.ClearInventory();
+                else player.ClearInventory(true, false);
+
+                Timing.CallDelayed(0.01f, () =>
+                {
+                    if (roleInventory.Items?.Count > 0)
+                        foreach (KeyValuePair<ItemType, int> Item in roleInventory.Items)
+                            if (Item.Value >= Random.Range(0, 101))
+                            {
+                                ItemBase itemBase = player.AddItem(Item.Key).Base;
+
+                                if (itemBase is Firearm firearm)
+                                {
+                                    if (AttachmentsServerHandler.PlayerPreferences.TryGetValue(player.ReferenceHub, out var value) && value.TryGetValue(itemBase.ItemTypeId, out var value2))
+                                        firearm.ApplyAttachmentsCode(value2, reValidate: true);
+
+                                    if (firearm.Modules.First(x => x is MagazineModule) is MagazineModule magazineModule)
+                                    {
+                                        magazineModule.ServerInsertEmptyMagazine();
+                                        magazineModule.AmmoStored = magazineModule.AmmoMax;
+                                        magazineModule.ServerResyncData();
+                                    }
+                                }
+                            }
+
+                    if (roleInventory.KeepAmmos && Ammos?.Count > 0)
+                        foreach (KeyValuePair<ItemType, ushort> ammo in Ammos)
+                            player.SetAmmo(ammo.Key, ammo.Value);
+
+                    if (roleInventory.Ammos?.Count > 0)
+                        foreach (KeyValuePair<ItemType, int> Ammo in roleInventory.Ammos)
+                            if (IsAmmo(Ammo.Key)) player.AddAmmo(Ammo.Key, (ushort)Ammo.Value);
+                });
+
+            }
+            catch (Exception e)
+            {
+                Logger.Error("[Event: SetPlayerInventory] " + e.ToString());
+            }
+        }
+
+        private string GetPlayerGroupName(Player player)
+        {
+            try
+            {
+                if (player.UserId == null || player.IsDummy) return string.Empty;
+
+                if (ServerStatic.PermissionsHandler.Members.ContainsKey(player.UserId))
+                    return ServerStatic.PermissionsHandler.Members[player.UserId];
+                else
+                    return player.UserGroup != null ? ServerStatic.PermissionsHandler.Groups.First(g => EqualsTo(g.Value, player.UserGroup)).Key : string.Empty;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("[InventoryControl] [Event: GetPlayerGroupName] " + e.ToString());
+                return string.Empty;
+            }
+        }
+
+        public static bool IsAmmo(ItemType type)
+        {
+            if (type == ItemType.Ammo9x19 || type == ItemType.Ammo762x39 || type == ItemType.Ammo556x45 || type == ItemType.Ammo44cal || type == ItemType.Ammo12gauge)
+                return true;
+            return false;
+        }
+
+        private bool EqualsTo(UserGroup check, UserGroup player)
+            => check.BadgeColor == player.BadgeColor
+               && check.BadgeText == player.BadgeText
+               && check.Permissions == player.Permissions
+               && check.Cover == player.Cover
+               && check.HiddenByDefault == player.HiddenByDefault
+               && check.Shared == player.Shared
+               && check.KickPower == player.KickPower
+               && check.RequiredKickPower == player.RequiredKickPower;
+    }
+}
---
+++ LabAPI/plugins/1984/InventoryControl-main/README.md
@@ -0,0 +1,205 @@
+# InventoryControl
+[![Version](https://img.shields.io/github/v/release/MrAfitol/InventoryControl?sort=semver&style=flat-square&color=blue&label=Version)](https://github.com/MrAfitol/InventoryControl/releases)
+[![Downloads](https://img.shields.io/github/downloads/MrAfitol/InventoryControl/total?style=flat-square&color=yellow&label=Downloads)](https://github.com/MrAfitol/InventoryControl/releases)
+
+A plugin that allows you to configure the default role inventory
+
+## How download ?
+   - *1. Find the SCP SL server config folder*
+   
+   *("C:\Users\(user name)\AppData\Roaming\SCP Secret Laboratory\" for windows, "/home/(user name)/.config/SCP Secret Laboratory/" for linux)*
+  
+   - *2. Find the "LabAPI" folder there, it contains the "plugins" folder.*
+  
+   - *3. Select either the port of your server to install the same on that server or the "global" folder to install the plugin for all servers*
+  
+  ***Or***
+  
+   - *Run the command in console `p install MrAfitol/InventoryControl`*
+
+## Config
+
+```yml
+# Custom inventory list for the role. (Do not add a role to the list if you want to leave the role as a regular inventory)
+inventory:
+  DefaultCassD:
+    role_type_id: ClassD
+    keep_items: false
+    keep_ammos: false
+    items:
+      Painkillers: 80
+      Coin: 100
+    ammos: 
+  JanitorCassD:
+    role_type_id: ClassD
+    keep_items: false
+    keep_ammos: false
+    items:
+      KeycardJanitor: 35
+      Painkillers: 80
+      Coin: 100
+    ammos: 
+  DefaultScientist:
+    role_type_id: Scientist
+    keep_items: true
+    keep_ammos: false
+    items:
+      Flashlight: 100
+      Coin: 90
+    ammos: 
+# Custom inventory list for players with a rank
+inventory_rank:
+  owner:
+    OwnerCassD:
+      role_type_id: ClassD
+      keep_items: false
+      keep_ammos: false
+      items:
+        KeycardScientist: 80
+        GunCOM18: 40
+        Painkillers: 100
+        Coin: 100
+      ammos:
+        Ammo9x19: 30
+    OwnerScientist:
+      role_type_id: Scientist
+      keep_items: true
+      keep_ammos: false
+      items:
+        GunCOM18: 65
+        SCP500: 70
+        Flashlight: 100
+        Coin: 90
+      ammos:
+        Ammo9x19: 60
+```
+
+
+```
+InventoryName:
+   role_type_id: RoleType
+   keep_items: true / false
+   keep_ammos: true / false
+   items:
+      ItemType: Chance
+   ammos:
+      AmmoType: Amount
+```
+
+```
+RankName:
+   InventoryName:
+      role_type_id: RoleType
+      keep_items: true / false
+      keep_ammos: true / false
+      items:
+         ItemType: Chance
+      ammos:
+         AmmoType: Amount
+```
+
+## Types
+
+**RoleType**
+```
+Scp173,
+ClassD,
+Spectator,
+Scp106,
+NtfSpecialist,
+Scp049,
+Scientist,
+Scp079,
+ChaosConscript,
+Scp096,
+Scp0492,
+NtfSergeant,
+NtfCaptain,
+NtfPrivate,
+Tutorial,
+FacilityGuard,
+Scp939,
+ChaosRifleman,
+ChaosRepressor,
+ChaosMarauder
+```
+
+**ItemType**
+```
+KeycardJanitor,         
+KeycardScientist,       
+KeycardResearchCoordinator,
+KeycardZoneManager,     
+KeycardGuard,           
+KeycardMTFPrivate,      
+KeycardContainmentEngineer,
+KeycardMTFOperative,    
+KeycardMTFCaptain,      
+KeycardFacilityManager, 
+KeycardChaosInsurgency, 
+KeycardO5,              
+Radio,                  
+GunCOM15,               
+Medkit,                 
+Flashlight,             
+MicroHID,               
+SCP500,                 
+SCP207,                 
+Ammo12gauge,            
+GunE11SR,               
+GunCrossvec,            
+Ammo556x45,             
+GunFSP9,                
+GunLogicer,             
+GrenadeHE,              
+GrenadeFlash,           
+Ammo44cal,              
+Ammo762x39,             
+Ammo9x19,               
+GunCOM18,               
+SCP018,                 
+SCP268,                 
+Adrenaline,             
+Painkillers,            
+Coin,                   
+ArmorLight,             
+ArmorCombat,            
+ArmorHeavy,             
+GunRevolver,            
+GunAK,                  
+GunShotgun,             
+SCP330,                 
+SCP2176,                
+SCP244a,                
+SCP244b,                
+SCP1853,                
+ParticleDisruptor,      
+GunCom45,               
+SCP1576,                
+Jailbird,               
+AntiSCP207,             
+GunFRMG0,               
+GunA7,                  
+Lantern,                
+SCP1344,                
+Snowball,               
+Coal,                   
+SpecialCoal,            
+SCP1507Tape,            
+DebugRagdollMover,      
+SurfaceAccessPass,      
+GunSCP127,              
+KeycardCustomTaskForce, 
+KeycardCustomSite02,    
+KeycardCustomManagement,
+KeycardCustomMetalCase
+```
+
+**AmmoType**
+```
+Ammo12gauge,
+Ammo556x45,
+Ammo44cal,
+Ammo762x39,
+Ammo9x19,
+```
